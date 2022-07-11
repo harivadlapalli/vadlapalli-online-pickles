126b5661981d04a1a98c533b329d559033eb8eaf
Hari <harivadlpalli1a@gmail.com>
2022-07-11 12:22:16 +0530

    first commit

---
+++ src/main/java/com/vadlapalli/onlinepickles/OnlinepicklesApplication.java
@@ -0,0 +1,13 @@
+package com.vadlapalli.onlinepickles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinepicklesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinepicklesApplication.class, args);
+	}
+
+}
